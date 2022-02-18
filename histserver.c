f5c4331c01648dba6695d96784404467b8764aaa
MUHAMMED CAN KÜÇÜKASLAN <muhammedcankucukaslan@gmail.com>
2022-02-19 01:08:03 +0300

    process file, send message to parent WIP
    The child now do process the file and sends the results to queue but currently it is not ensured that communication will be reliable both in terms of validity and completeness.

--- histserver.c
+++ histserver.c
@@ -88,10 +88,10 @@ int main(int argc, char **argv)
     }
 
     i = 0;
-    while(i <30) {
+    while(i <100000) {
         // processClientMQ();  
         processChildMQ(mq_c_s);   
-        printf("%d:--------------\n\n", i);     
+        //printf("%d:--------------\n\n", i);     
         // sendClientMQ();
         i++;
     }
@@ -109,11 +109,73 @@ int main(int argc, char **argv)
     return 0;       
 } // end of main method
 
+/** opens and process the file
+* opens the message queue
+* sends the result of file processing to the parent via the message queue
+* closes the message queue
+* finally, it terminates the child process.
+* @param filename the name of the file to be processed
+* @param intervalcount the number of intervals to be created
+* @param intervalwidth the width of each interval
+* @param intervalstart the start value of the first interval
+*/
 void child(char *filename, int intervalcount, int intervalwidth, int intervalstart)
 {
     printf("I am a child and mypid=%d\n", getpid());
     printf("Paremeters are %s, %d, %d, %d\n", filename, intervalcount, intervalwidth, intervalstart);
+    int i = 0;
 
+    // allocate memory for the array of intervals
+    // a number is counted in an interval indexed i 
+    // if number is in interval [i*intervalwidth, (i+1)*intervalwidth)
+    int *intervals = (int *)malloc(intervalcount * sizeof(int));
+    // initialize the array of intervals to 0 (better to be safe)
+    for (i = 0; i < intervalcount; ++i)
+        intervals[i] = 0;
+    
+    // try opening the file
+    // int file = open(filename, O_RDONLY);
+    FILE * file =  fopen (filename , "r");
+    if (file == NULL ) {
+            printf ("error in opening the file named %s\n", filename);
+            exit (1);
+    }
+
+    // read the file
+    // assuming it is   "an integer per line"
+    int hasNext;
+    char buf[1024];
+    // BEWARE THAT fgets reads until the new line character
+    // AND INCLUDES the new line character
+    // but DOES NOT INCLUDE the end of file character.
+    int number;
+    int number_of_lines = 0;
+    while(fgets (buf , 100 , file) != NULL) {
+        //hasNext = read(file, buf, 1024);
+        number = atoi(buf);
+        // find the interval in which the number is located
+        // and increment the corresponding interval
+        int interval = (number - intervalstart) / intervalwidth;
+        if (interval >= 0 && interval < intervalcount){
+            intervals[interval]++;
+            // TODO REMOVE DEBUG CODE 
+            // printf("%d is in interval %d\n", number, interval);
+        }
+        else
+            ;// todo uncomment? printf("number %d is not in interval [%d, %d)\n", number, intervalstart, intervalstart + intervalwidth * intervalcount);
+        if (number_of_lines % 100 == 0)
+        {
+            printf("%d lines read\n", number_of_lines);
+        }        
+        number_of_lines++;
+    }
+
+    // print the frequency of each interval
+    // with the interval start and end values
+    for (i = 0; i < intervalcount; ++i)
+        printf("interval [%d, %d) has %d numbers\n", intervalstart + i * intervalwidth, intervalstart + (i + 1) * intervalwidth, intervals[i]);
+
+    //  mq variables
     mqd_t mq_c_s;
 	struct mq_attr mq_c_s_attr;
 	struct ChildParentItem childParentItemPtr;
@@ -125,27 +187,33 @@ void child(char *filename, int intervalcount, int intervalwidth, int intervalsta
 		perror("Child cannot open msg queue FOR ChildParent\n");
 		exit(1);
 	}
-    printf("mq_c_s opened, mq_c_s id = %d\n", (int) mq_c_s);
+    printf("mq_c_s opened by child process, mq_c_s id = %d\n", (int) mq_c_s);
     
-    int i = 0;
 
     // create and send the messages
-    while(i < 5) {
-        i++;
+    i=0;
+    while(i < intervalcount) {
         childParentItemPtr.pid = getpid();
+        childParentItemPtr.interval = i;
+        childParentItemPtr.interval_frequency = intervals[i];
 
         int n = mq_send(mq_c_s, (char *) &childParentItemPtr, sizeof(struct ChildParentItem), 0);
 
         if (n == -1) {
             perror("mq_send failed\n");
-            exit(1);
         }
 
         printf("mq_send success, item size = %d\n",
                 (int) sizeof(struct ChildParentItem));
-        printf("childParentItemPtr->pid   = %d\n", childParentItemPtr.pid);
+        printf("childParentItemPtr->pid   = %d\n---------\n", childParentItemPtr.pid);
+                
+        i++;
     }
+
+    free(intervals);
     mq_close(mq_c_s);
+    printf("mq_c_s closed by child process, mq_c_s id = %d\n", (int) mq_c_s);
+
     exit(0); /* child is terminating */
 }
 
@@ -164,14 +232,16 @@ void processChildMQ(mqd_t mq) {
     int n;
     n = mq_receive(mq, (char *) bufptr, buflen, NULL);
     if (n == -1) {
-        perror("mq_receive failed\n");
-        
+//        perror("mq_receive failed\n");
     }
     else {
         printf("mq_receive success, message size = %d\n", n);
 
         struct ChildParentItem *itemptr = (struct ChildParentItem *) bufptr;
         printf("itemptr->pid = %d\n", itemptr->pid);
+        printf("itemptr->interval = %d\n", itemptr->interval);
+        printf("itemptr->interval_frequency = %d\n", itemptr->interval_frequency);
+        printf("-----------------endMessageReceive\n");
     }
     free(bufptr);
 }
--- shared_defs.h
+++ shared_defs.h
@@ -5,10 +5,14 @@ struct item {
 
 struct ChildParentItem {
 	int pid;
-//	int freq_size;
+	int interval;
+	int interval_frequency;
 	int freq[1000]; 
+	int status;
 };
 
+#define CHILD_CONTINUES 1
+#define CHILD_TERMINATED 0
 #define MQ_CLI_S "/21901779_a_CLIENT_SERVER"
 #define MQ_S_C "/21901779_a_SERVER_CLIENT"
 #define MQ_C_S "/21901779_a_CHILD_SERVER"
